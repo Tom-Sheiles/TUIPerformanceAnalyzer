5b396a7417edc8066f8b33f609c66a41bb7511d6
Tom-SH <tomassheiles@gmail.com>
2021-05-15 19:48:22 +1000

    Removed globals from main file and added header for definitions

---
+++ include.h
@@ -0,0 +1,9 @@
+#ifndef _INCLUDES_H
+#define _INCLUDES_H
+
+enum Windows{
+    PROCESSES_LIST,
+    MEMORY_VIEWER
+};
+
+#endif /* _INCLUDES_H */
--- main.c
+++ main.c
@@ -2,9 +2,7 @@
 #include <CConsole.h>
 #include <TlHelp32.h>
 
-
-int numberOfProcesses = 0;
-int state = 0;
+#include "include.h"
 
 typedef struct Process
 {
@@ -13,12 +11,13 @@ typedef struct Process
 
 } Process;
 
+
 //
 // Could be made more complex later. currently all processes are treated equally. 
 // Windows process can be identified with the IsProcessCritical function and values
 // sorted based on this
 //
-char **listProcesses(Console *console)
+char **listProcesses(Console *console, int *numberOfProcesses)
 {
     PROCESSENTRY32 procEntry;
     procEntry.dwSize = sizeof(PROCESSENTRY32);
@@ -27,14 +26,16 @@ char **listProcesses(Console *console)
 
     while(Process32Next(hProcSnap, &procEntry))
     {
-        numberOfProcesses++;
+        (*numberOfProcesses)++;
     }
-    numberOfProcesses -= 1;
+    (*numberOfProcesses) -= 1;
     Process32First(hProcSnap, &procEntry);
 
-
-    Process *processes = (Process *)malloc(sizeof(Process) * numberOfProcesses);
-    char **menuNames = malloc(sizeof(char *) * numberOfProcesses);
+    //
+    // MEMORY LEAKS FIX THIS
+    //
+    Process *processes = (Process *)malloc(sizeof(Process) * (*numberOfProcesses));
+    char **menuNames = malloc(sizeof(char *) * (*numberOfProcesses));
 
     int i = 0;
     while(Process32Next(hProcSnap, &procEntry))
@@ -51,7 +52,10 @@ char **listProcesses(Console *console)
 }
 
 
-int comp(const void *a, const void *b)
+//
+// Passed as pointer to qsort() to sort alphabetically
+//
+int alphaSort(const void *a, const void *b)
 {
     const char **ia = (const char **)a;
     const char **ib = (const char **)b;
@@ -65,42 +69,57 @@ int main()
     Console console;
     CreateConsole(&console);
 
-    Window processWindow = {console.bufferWidth * 0.3, 0, (console.bufferWidth - (console.bufferWidth*0.3) -console.bufferWidth*0.3), console.bufferHeight-2, BWHITE, "Processes", &console};
+    int currentWindow = PROCESSES_LIST;
 
-    char **menuItems = listProcesses(&console);
+    // PROCESSES LIST
+        Window processWindow = {console.bufferWidth * 0.3, 0, (console.bufferWidth - (console.bufferWidth*0.3) -console.bufferWidth*0.3), console.bufferHeight-2, BWHITE, "Processes", &console};
 
-    qsort(menuItems, numberOfProcesses, sizeof(char *), comp);
+        int numberOfProcesses = 0;
+        char **menuItems = listProcesses(&console, &numberOfProcesses);
 
-    Menu processesMenu = {5, 0, BWHITE, BACKGROUND_GREEN, numberOfProcesses};
-    ConsoleCreateMenu(&processesMenu, menuItems, &console, MENU_NOWRAP);
+        qsort(menuItems, numberOfProcesses, sizeof(char *), alphaSort);
+
+        Menu processesMenu = {5, 0, BWHITE, BACKGROUND_GREEN, numberOfProcesses};
+        ConsoleCreateMenu(&processesMenu, menuItems, &console, MENU_NOWRAP);
+        processesMenu.selected = 14;
+
+
+    // 
 
     Window memoryWindow = {1, 0, console.bufferWidth * 0.6, console.bufferHeight*0.70, BWHITE, "Memory", &console};
     Window memoryWindow2 = {console.bufferWidth*0.6+4, 0, (console.bufferWidth-(console.bufferWidth*0.6+5))-3, console.bufferHeight-1, BWHITE, "Two", &console};
 
-    processesMenu.selected = 14;
 
     int ticks = 0;
+
+    //
+    // Maybe should break the rules and split into a seperate program loop and draw call for each window to make logic more seperated.
+    //
     while(console.running)
     {
         Clear(&console);
         UpdateKeyState(&console);
-        if(console.keys['Q']) console.running = FALSE;
 
-        if(console.keys['D'] && processesMenu.selected <= numberOfProcesses-2)
-            {processesMenu.selected ++; processesMenu.y--;}
-        if(console.keys['E'] && processesMenu.selected > 0)
-            {processesMenu.selected --; processesMenu.y++;}
+        // Handle user input
+            if(console.keys['Q']) console.running = FALSE;
 
-        if(console.keys['A']) state = 1;
+            if(console.keys['D'] && processesMenu.selected <= numberOfProcesses-2)
+                {processesMenu.selected ++; processesMenu.y--;}
+            if(console.keys['E'] && processesMenu.selected > 0)
+                {processesMenu.selected --; processesMenu.y++;}
 
-        if(ticks % 50 == 0){
-            if(GetAsyncKeyState('S') && processesMenu.selected <= numberOfProcesses-2)
-            {processesMenu.selected ++; processesMenu.y--;}
-            if(GetAsyncKeyState('W') && processesMenu.selected > 0)
-            {processesMenu.selected --; processesMenu.y++;}
-        }
+            if(console.keys['A']) currentWindow = MEMORY_VIEWER;
 
-        if(state == 0){
+            if(ticks % 50 == 0){
+                if(GetAsyncKeyState('S') && processesMenu.selected <= numberOfProcesses-2)
+                {processesMenu.selected ++; processesMenu.y--;}
+                if(GetAsyncKeyState('W') && processesMenu.selected > 0)
+                {processesMenu.selected --; processesMenu.y++;}
+            }
+        //
+
+        if(currentWindow == PROCESSES_LIST)
+        {
             DrawString(&console, "[ ] Select Process", 3, 2, BWHITE);
             DrawString(&console, "[ ] Move Selection Up", 3, 4, BWHITE);
             DrawString(&console, "[ ] Move Selection Down", 3, 6, BWHITE);
@@ -114,31 +133,14 @@ int main()
             DrawWindow(processWindow);
             DrawMenuWindow(&processWindow, &processesMenu);
         }
-        else{
+        else if(currentWindow == MEMORY_VIEWER)
+        {
             DrawWindow(memoryWindow);
             DrawWindow(memoryWindow2);
 
-            WindowFillRect(&memoryWindow, FULL_PIXEL, 0, memoryWindow.h/2, 1, memoryWindow.h, LGREEN);
-            WindowFillRect(&memoryWindow, FULL_PIXEL, 1, memoryWindow.h/2, 1, memoryWindow.h, LGREEN);
-            
-            
-            WindowFillRect(&memoryWindow, FULL_PIXEL, 2, memoryWindow.h/2-1, 1, memoryWindow.h, LGREEN);
-            WindowFillRect(&memoryWindow, FULL_PIXEL, 3, memoryWindow.h/2-1, 1, memoryWindow.h, LGREEN);
-
-            WindowFillRect(&memoryWindow, FULL_PIXEL, 4, memoryWindow.h/2-3, 1, memoryWindow.h, LGREEN);
-            WindowFillRect(&memoryWindow, FULL_PIXEL, 5, memoryWindow.h/2-3, 1, memoryWindow.h, LGREEN);
-
-            WindowFillRect(&memoryWindow, FULL_PIXEL, 6, memoryWindow.h/2-3, 1, memoryWindow.h, LGREEN);
-            WindowFillRect(&memoryWindow, FULL_PIXEL, 7, memoryWindow.h/2-3, 1, memoryWindow.h, LGREEN);
-
-            WindowFillRect(&memoryWindow, FULL_PIXEL, 8, memoryWindow.h/2-3, 1, memoryWindow.h, LGREEN);
-            WindowFillRect(&memoryWindow, FULL_PIXEL, 9, memoryWindow.h/2-3, 1, memoryWindow.h, LGREEN);
-
-            WindowFillRect(&memoryWindow, FULL_PIXEL, 10, memoryWindow.h/2-5, 1, memoryWindow.h, LGREEN);
-            WindowFillRect(&memoryWindow, FULL_PIXEL, 11, memoryWindow.h/2-5, 1, memoryWindow.h, LGREEN);
-
-            WindowFillRect(&memoryWindow, FULL_PIXEL, 12, memoryWindow.h/2-5, 1, memoryWindow.h, LGREEN);
-            WindowFillRect(&memoryWindow, FULL_PIXEL, 13, memoryWindow.h/2-5, 1, memoryWindow.h, LGREEN);
+            WindowFillRect(&memoryWindow, FULL_PIXEL, 0, memoryWindow.h/2, 3, memoryWindow.h, LGREEN);
+            WindowFillRect(&memoryWindow, FULL_PIXEL, 3, memoryWindow.h/2-1, 6, memoryWindow.h, LGREEN);
+            WindowFillRect(&memoryWindow, FULL_PIXEL, 9, memoryWindow.h/2-5, 3, memoryWindow.h, LGREEN);
         }
 
         Draw(&console);
